8b97afec4d4fa61c14d6c51a7d46b31d705e9538
Dennis Whitley IV <whitley_dennis@bah.com>
2020-05-07 15:53:29 -0400

    fix: css on location picker and map

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 
 import 'leaflet/dist/leaflet.css';
 import App from './App';
+import styles from './App.module.css'
 
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render((
+  <div className={styles.router}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter></div>
 ), document.getElementById('root'));
